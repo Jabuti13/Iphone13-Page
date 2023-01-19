e989e84e5f0666cd7395c2449635031b9ed68ee1
Lucas Luan <celulin74@hotmail.com>
2023-01-19 03:27:32 -0300

    iphone-loja

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function imgSlider(e){
+    document.querySelector('#phone').src = e;
+}
+function mudarCorCircle(color){
+    const circle = document.querySelector('.circle')
+    circle.style.background = color
+}
